7baccefaf9bccd9e89b7d2e155e56c6f8616b22d
Juwelmia63 <designghor5@gmail.com>
2024-04-02 12:45:25 +0600

    form maked for resister

--- src/componant/Resister/Resister.jsx
+++ src/componant/Resister/Resister.jsx
@@ -3,7 +3,19 @@
 const Resister = () => {
     return (
         <div>
-            <h1 className="text-3xl text-center">This Is Resister Section </h1>
+            <div className="mx-auto md:w-1/2">
+                <h2 className="text-3xl mb-5 mt-2">
+            Please Resister
+                </h2>
+
+
+                <form>
+
+                   <input className="mb-4 w-3/4 py-2 px-4 bg-slate-200 rounded-xl" type="email" name="Email" placeholder="Email Address" id="" /> <br />
+                   <input className="mb-4 w-3/4 py-2 px-4 bg-slate-200 rounded-xl" type="password" name="Password" placeholder="Password" id="" /> <br />
+                   <input className="mb-4 w-3/4 py-2 px-4  rounded-xl btn btn-secondary" type="submit" value="Resister" />
+                </form>
+            </div>
         </div>
     );
 };
--- src/componant/Root/Root.jsx
+++ src/componant/Root/Root.jsx
@@ -2,12 +2,13 @@ import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 
 
+
 const Root = () => {
     return (
-        <div >
-
+        <div>
             <Header></Header>
             <Outlet></Outlet>
+           
         </div>
     );
 };
